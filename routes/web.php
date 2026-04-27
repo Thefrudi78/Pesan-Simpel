721fc921d5a1a458713073d729f7d7bdb1933f1a
Thefrudi78 <thefrudi78@gmail.com>
2026-04-28 00:57:33 +0700

    Rute login fixed

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', function () {
-    return view('login.index');
-})->name('login');
+Route::middleware('guest')->group(function () {
+    Route::get('/login', function () {
+        return view('login.index');
+    })->name('login');
 
-Route::post('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('login.post');
-Route::get('/', [App\Http\Controllers\DashboardController::class, 'index'])->name('chat');
+    Route::post('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('login.post');
+});
 
+Route::middleware('auth')->group(function () {
+    Route::get('/', [App\Http\Controllers\DashboardController::class, 'index'])->name('chat');
+    Route::post('/send', [App\Http\Controllers\DashboardController::class, 'send'])->name('send');
 
-Route::post('/send', [App\Http\Controllers\DashboardController::class, 'send'])->name('send');
-
-Route::post('/logout', function () {
-    Auth::logout();
-    return redirect('/');
-})->name('logout');
+    Route::post('/logout', function () {
+        Auth::logout();
+        return redirect('/');
+    })->name('logout');
+});
